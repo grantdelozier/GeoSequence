97d7792f4805302ff2de3a9e9c855ac09b47dac2
Grant DeLozier <grantdelozier@gmail.com>
2015-09-25 02:10:22 -0500

    add testing with viterbi discrim process

--- train_mav.py
+++ train_mav.py
@@ -545,7 +545,7 @@ def viterbi_discrim(obs, states, TM, LM):
 	path = {}
 
 	# Initialize base cases (t == 0)
-	print obs[0][-1]
+	#print obs[0][-1]
 	emission_dict = get_emission_dict(LM, obs[0][0])
 	for y in states:
 		V[0][y] = emission_dict[y]
